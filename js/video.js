d5fcf09e219da3534ee67926c9de92b65e2a71a5
Clarissa Araceli Diaz <143724246+clarissadiaz8@users.noreply.github.com>
2024-04-12 15:50:46 -0400

    Update video.js
    fix video volume

--- js/video.js
+++ js/video.js
@@ -13,7 +13,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
-	document.querySelector("#volume").innerHTML = "Volume: " + (video.volume * 100) + "%";
+	document.querySelector("#volume").innerHTML = (video.volume * 100) + "%";
 });
 
 // Pause the video
